f5885636d17343d14e85226c77c44858e608af4e
Linus Brogan <git@linusbrogan.com>
2021-10-04 20:34:30 -0700

    Convert hex to bytes

--- src/dev/brogan/cryptopals/S1C1.java
+++ src/dev/brogan/cryptopals/S1C1.java
@@ -4,4 +4,29 @@ public class S1C1 {
 	public static String convertHexToBase64(String hex) {
 		return null;
 	}
+
+	public static byte[] convertHexToBytes(String hex) {
+		int length = hex.length();
+		assert length % 1 != 1;
+		byte[] bytes = new byte[length / 2];
+		for (int i = 0; i < hex.length(); i += 2) {
+			char high = hex.charAt(i);
+			char low = hex.charAt(i + 1);
+			bytes[i / 2] = (byte) ((hexCharToByte(high) << 4) + hexCharToByte(low));
+		}
+		return bytes;
+	}
+
+	public static byte hexCharToByte(char c) {
+		if (c >= '0' && c <= '9') return (byte) (c - '0');
+		if (c >= 'a' && c <= 'f') return (byte) (c - 'a' + 10);
+		if (c >= 'A' && c <= 'F') return (byte) (c - 'A' + 10);
+
+		assert false;
+		return 0;
+	}
+
+	public static String convertBytesToBase64(byte[] bytes) {
+		return null;
+	}
 }
--- test/dev/brogan/cryptopals/S1C1Test.java
+++ test/dev/brogan/cryptopals/S1C1Test.java
@@ -7,9 +7,35 @@ import static org.junit.jupiter.api.Assertions.*;
 class S1C1Test {
 	private static final String BASE64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 	private static final String HEX = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+	private static String HEX_CHARS = "0123456789abcdef";
 
 	@Test
 	void convertsHexToBase64() {
 		assertEquals(BASE64, S1C1.convertHexToBase64(HEX));
+	}
+
+	@Test
+	void convertsHexToBytes() {
+		byte[] bytes = {1, 35, 69, 103, -119, -85, -51, -17};
+		assertArrayEquals(bytes, S1C1.convertHexToBytes(HEX_CHARS));
+		assertArrayEquals(bytes, S1C1.convertHexToBytes(HEX_CHARS.toUpperCase()));
+	}
+
+	@Test
+	void convertsHexCharToByte() {
+		char[] chars = HEX_CHARS.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			assertEquals(i, S1C1.hexCharToByte(chars[i]));
+		}
+
+		chars = HEX_CHARS.toUpperCase().toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			assertEquals(i, S1C1.hexCharToByte(chars[i]));
+		}
+	}
+
+	@Test
+	void convertsBytesToBase64() {
+		assert false;
 	}
 }
